798a2dae51a0a19851bb429e7bd3259ec0060ba2
Jose Fabiano Lima de Brito <143848948+jfabianobrito@users.noreply.github.com>
2023-11-09 18:04:31 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function trocaCor(cor){
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+
+function trocaImagem(imagem){
+    let imgIphone = document.querySelector(".iphone")
+    imgIphone.src = imagem
+}
